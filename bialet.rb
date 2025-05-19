4e98f9a39535ec5383e1e3cc4c4ad00de00ea3ac
Rodrigo Arce <albo@pragmore.com>
2025-05-19 00:22:35 -0300

    Update SHA256

--- bialet.rb
+++ bialet.rb
@@ -2,7 +2,7 @@ class Bialet < Formula
   desc "Bialet is a full-stack web framework made to enhance HTML with a native integration to a persistent database"
   homepage "https://github.com/bialet/bialet"
   url "https://github.com/bialet/bialet/archive/refs/tags/v0.8.tar.gz"
-  sha256 "b8a0c04be3b5718105cbe793465c9b30f417974621c72e0fa3649b1e439ab98b"
+  sha256 "03255468875b52505a6884a2d91ab77f2223c2ce4c491477137a7217bd30e958"
   license "MIT"
 
   depends_on "openssl@3"
