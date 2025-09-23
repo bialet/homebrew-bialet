02ae31968a15c07bafe5a32d594e78a5d2617d92
Rodrigo Arce <albo@pragmore.com>
2025-09-23 17:10:56 -0300

    Update version

--- bialet.rb
+++ bialet.rb
@@ -1,8 +1,8 @@
 class Bialet < Formula
   desc "Bialet is a full-stack web framework made to enhance HTML with a native integration to a persistent database"
   homepage "https://github.com/bialet/bialet"
-  url "https://github.com/bialet/bialet/archive/refs/tags/v0.8.tar.gz"
-  sha256 "03255468875b52505a6884a2d91ab77f2223c2ce4c491477137a7217bd30e958"
+  url "https://github.com/bialet/bialet/archive/refs/tags/v0.9.tar.gz"
+  sha256 "1cf83f9629e47fd9ed3dbab5379d4f667f1fa4935f743c8e1cfd6eed792dfa08"
   license "MIT"
 
   depends_on "openssl@3"
